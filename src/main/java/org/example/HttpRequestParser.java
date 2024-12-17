4bd991123be2a8d19ce10a89ce9ad3d2bdba4af2
Arun K <javamak@gmail.com>
2024-12-17 20:04:47 +0000

    body parsing fixed.

--- src/main/java/org/example/HttpRequestParser.java
+++ src/main/java/org/example/HttpRequestParser.java
@@ -20,54 +20,40 @@ public class HttpRequestParser {
 
     var httpRequest = new HttpRequest();
 
-    var available = inputStream.available();
-    BufferedReader reader = null;
-    if (available == 0) {
-      reader = new BufferedReader(new InputStreamReader((inputStream)));
-    } else {
-      var byteArr = new byte[available];
-      inputStream.read(byteArr);
-      reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(byteArr)));
-    }
-
-    parseRequest(reader, httpRequest);
-
-    return httpRequest;
-  }
-
-  private void parseRequest(BufferedReader reader, HttpRequest httpRequest)
-      throws IOException, HttpFormatException {
+    var reader = new BufferedReader(new InputStreamReader((inputStream)));
     var reqLine = reader.readLine();
     var arr = reqLine.split(" ");
     httpRequest.setHttpMethod(arr[0].trim());
     httpRequest.setRequestPath(arr[1].trim());
     httpRequest.setHttpVersion(arr[2].trim());
-    System.out.println(reqLine);
 
     String header = reader.readLine();
-    System.out.println(header);
+
     while (!header.isBlank()) {
       appendHeaderParameter(header, httpRequest);
       header = reader.readLine();
-      System.out.println(header);
+
     }
 
     readBody(reader, httpRequest);
-    System.out.println(httpRequest.getRequestPayload());
+
+    return httpRequest;
   }
+
 
   private void readBody(BufferedReader reader, HttpRequest request) throws IOException {
 
     if (request.getHeaders().containsKey("content-length")
         || request.getHeaders().containsKey("content-encoding")
         || request.getHeaders().containsKey("content-type")) {
-      String bodyLine = reader.readLine();
-      var sb = new StringBuilder();
-      while (bodyLine != null && !bodyLine.isBlank()) {
-        sb.append(bodyLine);
-        bodyLine = reader.readLine();
-      }
-      request.setRequestPayload(sb.toString());
+
+      var lengthStr = request.getHeaders().get("content-length");
+
+      var length = Integer.parseInt(lengthStr);
+
+      char[] charBuff = new char[length];
+      reader.read(charBuff);
+      request.setRequestPayload(new String(charBuff));
     }
   }
 
@@ -77,6 +63,6 @@ public class HttpRequestParser {
     if (idx == -1) {
       throw new HttpFormatException("Invalid Header Parameter: " + header);
     }
-    httpRequest.getHeaders().put(header.substring(0, idx).toLowerCase(), header.substring(idx + 1));
+    httpRequest.getHeaders().put(header.substring(0, idx).toLowerCase(), header.substring(idx + 1).trim());
   }
 }
